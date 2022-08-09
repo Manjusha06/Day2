f1deccbd2894bb612edede4ebbc3d81b83fce0b9
Manjusha Kandukuri <manjushakandukuri06@gmail.com>
2022-08-09 11:30:05 +0530

    changes done

---
+++ src/Array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Array {
+    public static void main( String args[])
+    {
+        int marks[] ={10,20,30,50,70,60,100 };
+        int max=0;
+        int min=marks[0];
+        for(int i =0; i<marks.length;i++)
+        {
+            if(marks[i]>max)
+            {
+                max=marks[i];
+            }
+        }
+        for(int i =0; i<marks.length;i++)
+        {
+            if(marks[i]<min)
+            {
+                min=marks[i];
+            }
+        }
+        System.out.println("Maximum marks " +max);
+        System.out.println("Minimum marks " +min);
+    }
+}
