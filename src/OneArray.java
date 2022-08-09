b235d856302dfe1daf932cb9ee03fec3ed63c1ee
Manjusha Kandukuri <manjushakandukuri06@gmail.com>
2022-08-09 13:11:55 +0530

    changes done

--- src/OneArray.java
+++ src/OneArray.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class OneArray {
+import java.util.Arrays;
+
+public class OneArray {
+    public static void main(String args[])
+    {
+        int[] arr = new int[]{ 1,2,3,4,3};
+        int sum=0;
+        for(int i=0;i<5;i++)
+        {
+            sum=sum+arr[i];
+        }
+        System.out.println("Length "+arr.length);
+        int mean=sum/5;
+        System.out.println("mean is "+mean);
+        Arrays.sort(arr);
+        double median=(arr.length)/2;
+        System.out.println("Median is "+median);
+        int maxVal=0, count=0, maxCount=0;
+
+        for(int i=0;i<5;i++) {
+            count = 0;
+            for(int j=0;j<5;j++) {
+                if(arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                maxVal = arr[i];
+            }
+        }
+        System.out.println("Mode is "+maxVal);
+
+    }
 }
--- src/Strings.java
+++ src/Strings.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Strings {
+public class Strings {
+    public static void main(String args[])
+    {
+        String str1="Manjusha";
+        String str2="Kandukuri";
+        int len=str1.length();
+        int len2=str2.length();
+        System.out.println(len);
+    }
 }
