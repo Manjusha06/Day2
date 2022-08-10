d86395367c82b5775567cc6d24984d2f4b9c8a14
Manjusha Kandukuri <manjushakandukuri06@gmail.com>
2022-08-10 12:45:19 +0530

    changes done

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -5,20 +5,16 @@ import static java.lang.Character.toUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class Assignment2 {
-    public static void main(String args[])
-    {
-        Scanner SC=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner SC = new Scanner(System.in);
         System.out.println("Enter a String");
-        String str=SC.nextLine();
-        String str1=" ";
-        for(int i=0;i<str.length();i++)
-        {
-            if(isLowerCase(str.charAt(i)))
-            {
-                str1=str1+toUpperCase(str.charAt(i));
-            }
-            else {
-                str1=str1+toLowerCase(str.charAt(i));
+        String str = SC.nextLine();
+        String str1 = " ";
+        for (int i = 0; i < str.length(); i++) {
+            if (isLowerCase(str.charAt(i))) {
+                str1 = str1 + toUpperCase(str.charAt(i));
+            } else {
+                str1 = str1 + toLowerCase(str.charAt(i));
             }
         }
         System.out.println(str1);
